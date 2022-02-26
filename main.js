bcb3ab213b813c08c152e0ffc860985b44129d60
Ej Palen <92970626+ejpalen@users.noreply.github.com>
2022-02-26 21:28:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -22,55 +22,63 @@ window.addEventListener("scroll", function () {
   nav.classList.toggle("scrolling-active", windowPosition);
 });
 
-var top1 = $("#about").offset().top;
-var top2 = $("#skills").offset().top;
-var top3 = $("#projects").offset().top;
-var top4 = $("#contact").offset().top;
+// var top1 = $("#about").offset().top;
+// var top2 = $("#skills").offset().top;
+// var top3 = $("#projects").offset().top;
+// var top4 = $("#contact").offset().top;
 
-$(document).scroll(function () {
-  var scrollPos = $(document).scrollTop();
-  if (scrollPos >= top1 && scrollPos < top2) {
-    $("#about-nav").css("background-color", "#6D7975");
-    $("#about-nav").css("color", "#fff");
-    $("#skills-nav").css("color", "#000");
-    $("#projects-nav").css("color", "#000");
-    $("#contact-nav").css("color", "#000");
+// $(document).scroll(function () {
+//   var scrollPos = $(document).scrollTop();
+//   if (scrollPos >= top1 && scrollPos < top2) {
+//     $("#about-nav").css("background-color", "#6D7975");
+//     $("#about-nav").css("color", "#fff");
+//     $("#skills-nav").css("color", "#000");
+//     $("#projects-nav").css("color", "#000");
+//     $("#contact-nav").css("color", "#000");
 
-    $("#skills-nav").css("background-color", "#fff");
-    $("#projects-nav").css("background-color", "#fff");
-    $("#contact-nav").css("background-color", "#fff");
-  } else if (scrollPos >= top2 && scrollPos < top3) {
-    $("#skills-nav").css("background-color", "#6D7975");
-    $("#about-nav").css("color", "#000");
-    $("#skills-nav").css("color", "#fff");
-    $("#projects-nav").css("color", "#000");
-    $("#contact-nav").css("color", "#000");
+//     $("#skills-nav").css("background-color", "#fff");
+//     $("#projects-nav").css("background-color", "#fff");
+//     $("#contact-nav").css("background-color", "#fff");
+//   } else if (scrollPos >= top2 && scrollPos < top3) {
+//     $("#skills-nav").css("background-color", "#6D7975");
+//     $("#about-nav").css("color", "#000");
+//     $("#skills-nav").css("color", "#fff");
+//     $("#projects-nav").css("color", "#000");
+//     $("#contact-nav").css("color", "#000");
 
-    $("#about-nav").css("background-color", "#fff");
-    $("#projects-nav").css("background-color", "#fff");
-    $("#contact-nav").css("background-color", "#fff");
-  } else if (scrollPos >= top3 && scrollPos < top4) {
-    $("#projects-nav").css("background-color", "#6D7975");
-    $("#about-nav").css("color", "#000");
-    $("#skills-nav").css("color", "#000");
-    $("#projects-nav").css("color", "#fff");
-    $("#contact-nav").css("color", "#000");
+//     $("#about-nav").css("background-color", "#fff");
+//     $("#projects-nav").css("background-color", "#fff");
+//     $("#contact-nav").css("background-color", "#fff");
+//   } else if (scrollPos >= top3 && scrollPos < top4) {
+//     $("#projects-nav").css("background-color", "#6D7975");
+//     $("#about-nav").css("color", "#000");
+//     $("#skills-nav").css("color", "#000");
+//     $("#projects-nav").css("color", "#fff");
+//     $("#contact-nav").css("color", "#000");
 
-    $("#about-nav").css("background-color", "#fff");
-    $("#skills-nav").css("background-color", "#fff");
-    $("#contact-nav").css("background-color", "#fff");
-  } else if (scrollPos >= top4) {
-    $("#contact-nav").css("background-color", "#6D7975");
-    $("#about-nav").css("color", "#000");
-    $("#skills-nav").css("color", "#000");
-    $("#projects-nav").css("color", "#000");
-    $("#contact-nav").css("color", "#fff");
+//     $("#about-nav").css("background-color", "#fff");
+//     $("#skills-nav").css("background-color", "#fff");
+//     $("#contact-nav").css("background-color", "#fff");
+//   } else if (scrollPos >= top4) {
+//     $("#contact-nav").css("background-color", "#6D7975");
+//     $("#about-nav").css("color", "#000");
+//     $("#skills-nav").css("color", "#000");
+//     $("#projects-nav").css("color", "#000");
+//     $("#contact-nav").css("color", "#fff");
 
-    $("#projects-nav").css("background-color", "#fff");
-    $("#about-nav").css("background-color", "#fff");
-    $("#skills-nav").css("background-color", "#fff");
-  } else if (scrollPos < top1) {
-    $("#about-nav").css("background-color", "#fff");
-    $("#about-nav").css("color", "#000");
-  }
-});
+//     $("#projects-nav").css("background-color", "#fff");
+//     $("#about-nav").css("background-color", "#fff");
+//     $("#skills-nav").css("background-color", "#fff");
+//   } else if (scrollPos < top1) {
+//     $("#about-nav").css("background-color", "#fff");
+//     $("#about-nav").css("color", "#000");
+//   }
+// });
+
+// let aboutNav = document
+//   .querySelector("#about-nav")
+//   .addEventListener("click", back);
+
+// function back() {
+//   aboutNav.classList.toggle("active");
+// }
